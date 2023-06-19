868a74609f9d8f76b445db1a6c67f273c8eb3e9c
nam.bui <baotran8810@gmail.com>
2023-06-19 10:16:41 +0700

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Bảo Trần on 19/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
